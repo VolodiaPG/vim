4a90dc549da0f056dc8e18a1df1ddf589d650dd8
Volodia P.-G. <volodia.parol-guarino@proton.me>
2024-02-26 14:11:23 +0100

    feat: add justfile support

--- flake.nix
+++ flake.nix
@@ -124,6 +124,9 @@
               lsp-format.enable = true;
             };
             globals.mapleader = " ";
+            extraPlugins = with pkgs.vimPlugins;[
+              vim-just
+            ];
             extraConfigLua= ''
               local lspconfig = require('lspconfig')
               local capabilities = require('cmp_nvim_lsp').default_capabilities()
