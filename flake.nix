614dd706cabef2529a67684483fa3845219cfc61
Volodia P.-G. <volodia.parol-guarino@proton.me>
2024-02-26 22:18:22 +0100

    refactor: change lsp by coc

--- flake.nix
+++ flake.nix
@@ -33,17 +33,6 @@
             plugins.nix-develop.enable = true;
             plugins.trouble.enable = true;
             plugins.treesitter.enable = true;
-            plugins.cmp-treesitter.enable = true;
-            plugins.treesitter-refactor = {
-              enable = true;
-              highlightCurrentScope.enable = true;
-              highlightCurrentScope.disable = [
-                "nix"
-              ];
-              highlightDefinitions.enable = true;
-              navigation.enable = true;
-              smartRename.enable = true;
-            };
             plugins.telescope.enable = true;
             plugins.telescope.keymaps = {
               "<leader>ff" = "find_files";
@@ -72,98 +61,23 @@
                   };
                 };
               };
-              lsp = {
-                enable = true;
-                servers = {
-                  bashls.enable = true;
-                  nixd.enable = true;
-                  pyright.enable = true;
-                  gopls.enable = true;
-                  rust-analyzer = {
-                    enable = true;
-                    installRustc = false;
-                    installCargo = false;
-                  };
-                  jsonls.enable = true;
-                  yamlls.enable = true;
-                };
-                #lspBuf = {
-                #  "gd" = "definition";
-                #  "gD" = "references";
-                #  "gt" = "type_definition";
-                #  "gi" = "implementation";
-                #  "K" = "hover";
-                #};
-              };
-              rust-tools = {
-                enable = true;
-                inlayHints = {
-                  auto = true;
-                  onlyCurrentLine = false;
-                  showParameterHints = true;
-                  parameterHintsPrefix = "<- ";
-                  otherHintsPrefix = "=> ";
-                  maxLenAlign = false;
-                  maxLenAlignPadding = 1;
-                  rightAlign = false;
-                  rightAlignPadding = 7;
-                  highlight = "Comment";
-                };
-              };
-              nvim-cmp = {
-                enable = true;
-                snippet.expand = "luasnip";
-                sources = [
-                  {name = "nvim_lsp";}
-                  {name = "path";}
-                  {name = "buffer";}
-                ];
-                mapping = {
-                  "<CR>" = "cmp.mapping.confirm({select = true})";
-                  "<Tab>" = ''cmp.mapping(cmp.mapping.select_next_item(), {"i", "s"})'';
-                  "<S-Tab>" = ''cmp.mapping(cmp.mapping.select_prev_item(), {"i", "s"})'';
-                };
-              };
-              luasnip.enable = true;
               gitsigns.enable = true;
-              lspkind = {
-                enable = true;
-                cmp = {
-                  enable = true;
-                  menu = {
-                    nvim_lsp = "[LSP]";
-                    nvim_lua = "[api]";
-                    path = "[path]";
-                    luasnip = "[snip]";
-                    buffer = "[buffer]";
-                    orgmode = "[orgmode]";
-                    neorg = "[neorg]";
-                  };
-                };
-              };
-              lspsaga = {
-                enable = true;
-                lightbulb.sign = false;
-              };
-              lsp-lines.enable = true;
-              lsp-format.enable = true;
             };
             globals.mapleader = " ";
             extraPlugins = with pkgs.vimPlugins; [
               vim-just
-              virtual-types-nvim
+              coc-highlight
+              coc-pyright
+              coc-rust-analyzer
+              coc-go
+              coc-spell-checker
+              coc-nvim
+              coc-sh
+              coc-yaml
+              coc-toml
+              coc-r-lsp
+              coc-diagnostic
             ];
-            extraConfigLua = ''
-              local lspconfig = require('lspconfig')
-              local capabilities = require('cmp_nvim_lsp').default_capabilities()
-              lspconfig.r_language_server.setup({
-                on_attach = on_attach_custom,
-                -- Debounce "textDocument/didChange" notifications because they are slowly
-                -- processed (seen when going through completion list with `<C-N>`)
-                flags = { debounce_text_changes = 150 },
-                capabilities = capabilities,
-              })
-            '';
           };
           nixvim' = nixvim.legacyPackages."${system}";
           nvim = nixvim'.makeNixvim config;
