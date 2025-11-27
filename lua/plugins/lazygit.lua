fa5d693e743c51e92df8c0f9069ec7e706e3991a
Volodia P.G. <volodia.parol-guarino@proton.me>
2025-11-27 15:02:10 +0100

    chore: reformat

--- lua/plugins/lazygit.lua
+++ lua/plugins/lazygit.lua
@@ -1,24 +1,25 @@
 -- LazyGit plugin configuration
 
 return {
-  "kdheepak/lazygit.nvim",
+  'kdheepak/lazygit.nvim',
   dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-telescope/telescope.nvim",
+    'nvim-lua/plenary.nvim',
+    'nvim-telescope/telescope.nvim',
   },
   config = function()
     -- Load the telescope extension
-    require("telescope").load_extension("lazygit")
-    
+    require('telescope').load_extension 'lazygit'
+
     -- Set up keymaps
-    vim.keymap.set("n", "<leader>gg", "<cmd>LazyGit<CR>", {
-      desc = "LazyGit (root dir)",
+    vim.keymap.set('n', '<leader>gg', '<cmd>LazyGit<CR>', {
+      desc = 'LazyGit (root dir)',
     })
-    
-    vim.keymap.set("n", "<leader>gG", function()
-      vim.cmd("Telescope lazygit")
+
+    vim.keymap.set('n', '<leader>gG', function()
+      vim.cmd 'Telescope lazygit'
     end, {
-      desc = "LazyGit repositories",
+      desc = 'LazyGit repositories',
     })
   end,
-} 
+}
+
