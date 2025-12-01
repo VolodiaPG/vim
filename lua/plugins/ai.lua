a75898120a5efbb221b6a1a6a4a6efe0278928b2
Volodia P.G. <volodia.parol-guarino@proton.me>
2025-12-01 09:23:37 +0100

    feat: add ai correction

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -9,7 +9,8 @@ local prompt_library = {
           local text = require('codecompanion.helpers.actions').get_code(context.start_line, context.end_line)
 
           return [[
-          I want you act as a proofreader and experienced academic writer. I will provide you texts and I would like you to review them, and rephrase them to provide a better version accoirding to the folowing standards:
+          I want you act as a proofreader and experienced academic writer.
+          I will provide you texts and I would like you to review them, and rephrase them to provide a better version according to the folowing standards:
 
           - Correct any spelling, grammar, or punctuation errors.
           - Do not use any contractions like I'm or you're.
@@ -37,6 +38,51 @@ local prompt_library = {
     opts = {
       modes = { 'v' },
       short_name = 'rewrite',
+      auto_submit = true,
+      stop_context_insertion = true,
+    },
+  },
+  ['Academic correct'] = {
+    strategy = 'inline',
+    description = 'Write better academic papers for me',
+    prompts = {
+      {
+        role = 'user',
+        content = function(context)
+          local text = require('codecompanion.helpers.actions').get_code(context.start_line, context.end_line)
+
+          return [[
+          I want you act as a proofreader and experienced academic writer.
+          I will provide you texts and I would like you to review them, and rephrase them as little as possible to provide a better version according to the folowing standards:
+
+          - Correct any spelling, grammar, or punctuation errors.
+          - Do not use any contractions like I'm or you're.
+          - Just send me the revised text without anything else.
+
+          Also beware to make your response follow these requirements:
+
+          *   **Feel:** No language or vocabulary that would make the output feel like AI generated.
+          *   **Sentence Length:** A mix of short and medium-length sentences.
+          *   **Vocabulary:** As precise as possible.
+          *   **Voice:** Use the active voice.
+          *   **Content:** The idea should be clear and concise.
+          *   **Flow:** The ideas should flow logically.
+          *   **Latex:** Dot not change the custom LaTeX commands that may be used.
+
+          Most importantly: you must change the orginal text the least possible.
+
+          The text is here
+
+          ```
+          ]] .. text .. [[
+          ```
+          ]]
+        end,
+      },
+    },
+    opts = {
+      modes = { 'v' },
+      short_name = 'correct',
       auto_submit = true,
       stop_context_insertion = true,
     },
@@ -111,6 +157,7 @@ return {
       { '<leader>c', ':CodeCompanionChat toggle<CR>', desc = 'AI Chat' },
       { '<leader>A', ':CodeCompanionActions<CR>', desc = 'AI Actions' },
       { '<leader>W', ':CodeCompanion /rewrite<CR>', desc = 'AI rewrite' },
+      { '<leader>C', ':CodeCompanion /correct<CR>', desc = 'AI correction' },
     }
   end,
 }
