135a3c22466c7c108dbd889d8c8629d20ce48595
Volodia P.G. <volodia.parol-guarino@proton.me>
2025-12-03 13:59:52 +0100

    feat: make noice not use animations

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,16 +1,33 @@
 -- lazy.nvim
 return {
-  'folke/noice.nvim',
-  event = 'VeryLazy',
-  opts = {
-    -- add any options here
-  },
-  dependencies = {
-    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-    'MunifTanjim/nui.nvim',
-    -- OPTIONAL:
-    --   `nvim-notify` is only needed, if you want to use the notification view.
-    --   If not available, we use `mini` as the fallback
+  {
     'rcarriga/nvim-notify',
+    keys = {
+      {
+        '<Leader>un',
+        function()
+          require('notify').dismiss { silent = true, pending = true }
+        end,
+        desc = 'Dismiss all notifications (Notify)',
+      },
+    },
+    opts = {
+      stages = 'static',
+    },
+  },
+  {
+    'folke/noice.nvim',
+    event = 'VeryLazy',
+    opts = {
+      -- add any options here
+    },
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      'MunifTanjim/nui.nvim',
+      -- OPTIONAL:
+      --   `nvim-notify` is only needed, if you want to use the notification view.
+      --   If not available, we use `mini` as the fallback
+      'rcarriga/nvim-notify',
+    },
   },
 }
