9954c3cab3e1f1f3ffdfb312ab6455d320c0c272
Volodia P.G. <volodia.parol-guarino@proton.me>
2025-12-10 11:29:49 +0100

    feat: add buffer  management

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,13 +1,12 @@
 -- Telescope plugin configuration
 
 return {
-  "nvim-telescope/telescope.nvim",
+  'nvim-telescope/telescope.nvim',
   dependencies = {
-    "nvim-lua/plenary.nvim",
-    -- { "nvim-telescope/telescope-fzf-native.nvim", build=false},
-    "nvim-tree/nvim-web-devicons",
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons',
   },
-  cmd = "Telescope",
+  cmd = 'Telescope',
   -- keys = {
   --   { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find files" },
   --   { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live grep" },
@@ -23,18 +22,20 @@ return {
   --   { "<leader>ft", "<cmd>Telescope search_history<cr>", desc = "Search history" },
   -- },
   config = function()
-    local telescope = require("telescope")
-    local actions = require("telescope.actions")
+    local telescope = require 'telescope'
+    local actions = require 'telescope.actions'
+    local builtin = require 'telescope.builtin'
+    local action_state = require 'telescope.actions.state'
 
-    telescope.setup({
+    telescope.setup {
       defaults = {
-        prompt_prefix = " ",
-        selection_caret = " ",
-        path_display = { "truncate" },
-        sorting_strategy = "ascending",
+        prompt_prefix = ' ',
+        selection_caret = ' ',
+        path_display = { 'truncate' },
+        sorting_strategy = 'ascending',
         layout_config = {
           horizontal = {
-            prompt_position = "top",
+            prompt_position = 'top',
             preview_width = 0.55,
             results_width = 0.8,
           },
@@ -47,52 +48,52 @@ return {
         },
         mappings = {
           i = {
-            ["<C-n>"] = actions.cycle_history_next,
-            ["<C-p>"] = actions.cycle_history_prev,
-            ["<C-j>"] = actions.move_selection_next,
-            ["<C-k>"] = actions.move_selection_previous,
-            ["<C-c>"] = actions.close,
-            ["<Down>"] = actions.move_selection_next,
-            ["<Up>"] = actions.move_selection_previous,
-            ["<CR>"] = actions.select_default,
-            ["<C-x>"] = actions.select_horizontal,
-            ["<C-v>"] = actions.select_vertical,
-            ["<C-t>"] = actions.select_tab,
-            ["<C-u>"] = actions.preview_scrolling_up,
-            ["<C-d>"] = actions.preview_scrolling_down,
-            ["<PageUp>"] = actions.results_scrolling_up,
-            ["<PageDown>"] = actions.results_scrolling_down,
-            ["<Tab>"] = actions.toggle_selection + actions.move_selection_worse,
-            ["<S-Tab>"] = actions.toggle_selection + actions.move_selection_better,
-            ["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
-            ["<M-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-            ["<C-l>"] = actions.complete_tag,
-            ["<C-_>"] = actions.which_key, -- keys from pressing <C-/>
+            ['<C-n>'] = actions.cycle_history_next,
+            ['<C-p>'] = actions.cycle_history_prev,
+            ['<C-j>'] = actions.move_selection_next,
+            ['<C-k>'] = actions.move_selection_previous,
+            ['<C-c>'] = actions.close,
+            ['<Down>'] = actions.move_selection_next,
+            ['<Up>'] = actions.move_selection_previous,
+            ['<CR>'] = actions.select_default,
+            ['<C-x>'] = actions.select_horizontal,
+            ['<C-v>'] = actions.select_vertical,
+            ['<C-t>'] = actions.select_tab,
+            ['<C-u>'] = actions.preview_scrolling_up,
+            ['<C-d>'] = actions.preview_scrolling_down,
+            ['<PageUp>'] = actions.results_scrolling_up,
+            ['<PageDown>'] = actions.results_scrolling_down,
+            ['<Tab>'] = actions.toggle_selection + actions.move_selection_worse,
+            ['<S-Tab>'] = actions.toggle_selection + actions.move_selection_better,
+            ['<C-q>'] = actions.send_to_qflist + actions.open_qflist,
+            ['<M-q>'] = actions.send_selected_to_qflist + actions.open_qflist,
+            ['<C-l>'] = actions.complete_tag,
+            ['<C-_>'] = actions.which_key, -- keys from pressing <C-/>
           },
           n = {
-            ["<esc>"] = actions.close,
-            ["<CR>"] = actions.select_default,
-            ["<C-x>"] = actions.select_horizontal,
-            ["<C-v>"] = actions.select_vertical,
-            ["<C-t>"] = actions.select_tab,
-            ["<Tab>"] = actions.toggle_selection + actions.move_selection_worse,
-            ["<S-Tab>"] = actions.toggle_selection + actions.move_selection_better,
-            ["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
-            ["<M-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-            ["j"] = actions.move_selection_next,
-            ["k"] = actions.move_selection_previous,
-            ["H"] = actions.move_to_top,
-            ["M"] = actions.move_to_middle,
-            ["L"] = actions.move_to_bottom,
-            ["<Down>"] = actions.move_selection_next,
-            ["<Up>"] = actions.move_selection_previous,
-            ["gg"] = actions.move_to_top,
-            ["G"] = actions.move_to_bottom,
-            ["<C-u>"] = actions.preview_scrolling_up,
-            ["<C-d>"] = actions.preview_scrolling_down,
-            ["<PageUp>"] = actions.results_scrolling_up,
-            ["<PageDown>"] = actions.results_scrolling_down,
-            ["?"] = actions.which_key,
+            ['<esc>'] = actions.close,
+            ['<CR>'] = actions.select_default,
+            ['<C-x>'] = actions.select_horizontal,
+            ['<C-v>'] = actions.select_vertical,
+            ['<C-t>'] = actions.select_tab,
+            ['<Tab>'] = actions.toggle_selection + actions.move_selection_worse,
+            ['<S-Tab>'] = actions.toggle_selection + actions.move_selection_better,
+            ['<C-q>'] = actions.send_to_qflist + actions.open_qflist,
+            ['<M-q>'] = actions.send_selected_to_qflist + actions.open_qflist,
+            ['j'] = actions.move_selection_next,
+            ['k'] = actions.move_selection_previous,
+            ['H'] = actions.move_to_top,
+            ['M'] = actions.move_to_middle,
+            ['L'] = actions.move_to_bottom,
+            ['<Down>'] = actions.move_selection_next,
+            ['<Up>'] = actions.move_selection_previous,
+            ['gg'] = actions.move_to_top,
+            ['G'] = actions.move_to_bottom,
+            ['<C-u>'] = actions.preview_scrolling_up,
+            ['<C-d>'] = actions.preview_scrolling_down,
+            ['<PageUp>'] = actions.results_scrolling_up,
+            ['<PageDown>'] = actions.results_scrolling_down,
+            ['?'] = actions.which_key,
           },
         },
       },
@@ -106,12 +107,49 @@ return {
           fuzzy = true,
           override_generic_sorter = true,
           override_file_sorter = true,
-          case_mode = "smart_case",
+          case_mode = 'smart_case',
         },
       },
-    })
+    }
 
     -- Load extensions
-    -- telescope.load_extension("fzf")
+    -- telescope.load_extension 'fzf'
+
+    buffer_searcher = function()
+      builtin.buffers {
+        sort_mru = true,
+        ignore_current_buffer = true,
+        show_all_buffers = false,
+        attach_mappings = function(prompt_bufnr, map)
+          local refresh_buffer_searcher = function()
+            actions.close(prompt_bufnr)
+            vim.schedule(buffer_searcher)
+          end
+
+          local delete_buf = function()
+            local selection = action_state.get_selected_entry()
+            vim.api.nvim_buf_delete(selection.bufnr, { force = true })
+            refresh_buffer_searcher()
+          end
+
+          local delete_multiple_buf = function()
+            local picker = action_state.get_current_picker(prompt_bufnr)
+            local selection = picker:get_multi_selection()
+            for _, entry in ipairs(selection) do
+              vim.api.nvim_buf_delete(entry.bufnr, { force = true })
+            end
+            refresh_buffer_searcher()
+          end
+
+          map('n', 'dd', delete_buf)
+          map('n', '<C-d>', delete_multiple_buf)
+          map('i', '<C-d>', delete_multiple_buf)
+
+          return true
+        end,
+      }
+    end
+
+    vim.keymap.set('n', '<leader>fb', buffer_searcher, {})
   end,
 }
