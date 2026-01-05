44b7ec77037753f30306194292901c5568231861
Volodia P.G. <volodia.parol-guarino@proton.me>
2026-01-05 12:01:25 +0100

    feat: enable gitsigns in the numbers on the left

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -14,7 +14,7 @@ return {
         untracked = { text = '┆' },
       },
       signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
-      numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
+      numhl = true, -- Toggle with `:Gitsigns toggle_numhl`
       linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
       word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
       watch_gitdir = {
@@ -99,4 +99,3 @@ return {
     }
   end,
 }
-
